fdbe65f5401bba7e04cd1ea92ab4d7ab9e8c77a5
seventh1976 <seventh1976@gmail.com>
2020-02-27 14:59:47 +0000

    Added MongoDB Atlas connection

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes/api');
+const path = require('path');
 require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+// connect to the database
+mongoose.connect(process.env.DB, {
+  dbName: 'MERNTodo',
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+  }).then(() => console.log(`Database connected successfully`))
+    .catch(err => console.log(err));
+
+mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -11,8 +25,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use((req, res, next) => {
-  res.send('Welcome to Express');
+app.use(bodyParser.json());
+
+app.use('/api', routes);
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  next();
 });
 
 app.listen(port, () => {
